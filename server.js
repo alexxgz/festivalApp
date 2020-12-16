4dc9bce3cc536f0ed89a2180c96863bdc06ebbff
alexgz <alexinc19@gmail.com>
2020-12-16 12:11:57 -0800

    -Set up server

---
+++ server.js
@@ -0,0 +1,46 @@
+//External Modules
+const express = require("express");
+const methodOverride = require("method-override");
+
+//Internal Modules
+const controllers = require("./controllers");
+
+//Instanced Modules
+const app = express();
+
+//Configuration
+const PORT = 4000;
+app.set("view engine", "ejs");
+
+
+//Middleware
+
+// server public as static files
+// express.static(directory location absolute)
+app.use(express.static(__dirname + '/public'));
+
+// body data middleware
+app.use(express.urlencoded({ extended: true }));
+// method override middleware
+app.use(methodOverride("_method"));
+
+
+
+
+
+//Home 
+app.get("/", function (req, res) {
+    res.render("home");
+});
+
+
+//404 error
+app.get(function (request, response) {
+    response.send("404 -This does not exist in the universe you are in-")
+});
+
+
+//Server listner
+app.listen(PORT, function () {
+    console.log(`Festival is live at http://localhost:${PORT}/`)
+});
